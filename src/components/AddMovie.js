42b1ac04366307f55af5a6074e7db650ebab59a4
gordongest <gordon.gest@gmail.com>
2020-10-22 10:59:13 -0500

    forgot to push AddMovie.js to repo

---
+++ src/components/AddMovie.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class AddMovie extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputVal: ''
+    }
+
+    this.handleChange = this.props.handleChange.bind(this)
+  }
+
+  render() {
+
+    return (
+
+      <div className="add-movie-bar">
+            <input type="text"
+              onChange={this.handleChange}
+              placeholder="Add a movie..." />
+            <button
+              className="add-button"
+              type="submit"
+              onClick={() => {this.props.handleAddSubmit(this.state.inputVal)}}>
+              Add
+            </button>
+        </div>
+
+    )
+
+  }
+
+}
+
+export default AddMovie;
